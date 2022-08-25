93feb216c98e956ef238e788c6fc6319c869c775
HuseinliAli <ali.guseynoov@gmail.com>
2022-08-25 12:22:21 +0400

    Structure of dictionary

---
+++ Dictionary/Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class MyDictionary<TMyKey , TMyValue>
+    {
+        TMyKey[] keys;
+        TMyValue[] values;
+        
+        public MyDictionary()
+        {
+            keys = new TMyKey[0];
+            values = new TMyValue[0];
+        }
+
+        public void Add(TMyKey key, TMyValue value)
+        {
+            TMyKey[] myKeys = keys;
+            keys = new TMyKey[keys.Length+1];
+            TMyValue[] myValues = values;
+            values = new TMyValue[myValues.Length+1];
+
+            for(int i =0; i< myKeys.Length; i++)
+            {
+                keys[i] = myKeys[i];
+                values[i] = myValues[i];
+            }
+            keys[keys.Length-1] = key;
+            values[values.Length-1] = value;
+        }
+
+        public void List()
+        {
+            for(int i =0; i<keys.Length; i++)
+            {
+                Console.WriteLine(keys[i]+" "+values[i]);
+            }
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int ,string> dic = new MyDictionary<int , string>();
+            dic.Add(1, "Beni Bana Baglar");
+            dic.Add(2, "Nasilsiniz Agalar");
+            dic.List();
+        }
+    }
+}
