8db3614f6fd0c29161223c7988278d8e7344f6d9
HuseinliAli <ali.guseynoov@gmail.com>
2022-09-20 06:09:44 +0400

    WebAPI

---
+++ FinalProject/SebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //loosely cupled
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this._productService=productService;
+        }
+
+        [HttpGet]
+        public List<Product>Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
