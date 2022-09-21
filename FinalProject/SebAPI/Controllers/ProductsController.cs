019ae5aa3bc7dc8c0d37e9c982da93a11e8ad358
HuseinliAli <ali.guseynoov@gmail.com>
2022-09-21 08:21:07 +0400

    WebAPI added

--- FinalProject/SebAPI/Controllers/ProductsController.cs
+++ FinalProject/SebAPI/Controllers/ProductsController.cs
@@ -5,26 +5,59 @@ using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
 
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
     public class ProductsController : ControllerBase
     {
         //loosely cupled
-        IProductService _productService;
+        public IProductService _productService;
 
         public ProductsController(IProductService productService)
         {
             this._productService=productService;
         }
 
-        [HttpGet]
-        public List<Product>Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             var result = _productService.GetAll();
-            return result.Data;
+
+            if(result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetByID(int id)
+        {
+            var result = _productService.GetByID(id);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
         }
     }
 }
