6290d0999aff2522d1579e4cfe1e8d79d2d1278f
HuseinliAli <ali.guseynoov@gmail.com>
2022-09-24 11:42:40 +0400

    Autofac is added project

---
+++ FinalProject/TestWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService=productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+            if(result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetByID(int id)
+        {
+            var result = _productService.GetByID(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
